9c7366849311829f9ecbae0643545fbe0c3553d0
황민도 <167045161+hwangmindo@users.noreply.github.com>
2024-08-21 15:01:17 +0900

    Setup project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ShowModule } from './show/show.module';
+import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ShowModule, BookModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
